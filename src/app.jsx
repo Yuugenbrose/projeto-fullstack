3ceb5094a8726b059b6a156200041b1fda36e9ea
Matheus-Barreto06 <164064946+Matheus-Barreto06@users.noreply.github.com>
2025-05-16 21:01:16 -0300

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Formulario';
+import WeatherDisplay from './components/WeatherDisplay'; // Importar
 
 const App = () => {
   return (
     <div>
       <Formulario />
+      <WeatherDisplay /> {/* Adicionar */}
     </div>
   );
 };
